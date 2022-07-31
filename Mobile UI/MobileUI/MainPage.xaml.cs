3a7378b6cf40ded3b3f1e6c2c1805bf696d08f8c
Anandhuraju <77533028+Anandhuraju@users.noreply.github.com>
2022-07-31 09:23:29 +0530

    Add files via upload

---
+++ Mobile UI/MobileUI/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileUI
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool m_BtnClicked;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //Navigating to next
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            if (!m_BtnClicked)
+            {
+                m_BtnClicked = true;
+
+                Navigation.PushAsync(new MyPolicies());
+
+            }
+            else
+            {
+                m_BtnClicked = false;
+            }
+
+        }
+    }
+}
---
+++ Mobile UI/MobileUI/MyPolicies.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileUI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPolicies : ContentPage
+    {
+        public MyPolicies()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
